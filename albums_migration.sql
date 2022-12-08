3ed29c0fccc09e1bb14faacc88d26a8c5862ba50
J.J. Jones <jacob.j.jones01@gmail.com>
2022-12-08 10:02:26 -0600

    Working

--- albums_migration.sql
+++ albums_migration.sql
@@ -1,1 +1,12 @@
-CREATE DATABASE IF NOT EXISTS testing123;
+use codeup_test_db;
+CREATE USER 'codeup_test_user'@'localhost' IDENTIFIED BY 'codeup';
+GRANT ALL ON 'codeup_test_db' TO 'codeup_test_user'@'localhost';
+GRANT CREATE, ALTER, INSERT, DROP ON codeup_test_db.* TO 'codeup_test_user'@'localhost';
+create table album(
+    id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+    artist VARCHAR(20),
+    name VARCHAR(20),
+    release_date DATE,
+    sales FLOAT,
+    genre VARCHAR(20)
+);
