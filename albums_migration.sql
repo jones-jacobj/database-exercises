1fd29db082c03ad93617e70cd2a8c90e0e163887
J.J. Jones <jacob.j.jones01@gmail.com>
2022-12-08 14:39:58 -0600

    Updated Albums

--- albums_migration.sql
+++ albums_migration.sql
@@ -1,12 +1,14 @@
-use codeup_test_db;
-CREATE USER 'codeup_test_user'@'localhost' IDENTIFIED BY 'codeup';
+use 'codeup_test_db';
+-- CREATE USER 'codeup_test_user'@'localhost' IDENTIFIED BY 'codeup';
 GRANT ALL ON 'codeup_test_db' TO 'codeup_test_user'@'localhost';
-GRANT CREATE, ALTER, INSERT, DROP ON codeup_test_db.* TO 'codeup_test_user'@'localhost';
+GRANT CREATE, ALTER, INSERT, DROP ON *.* TO 'codeup_test_user'@'localhost';
 create table album(
     id INT UNSIGNED NOT NULL AUTO_INCREMENT,
-    artist VARCHAR(20),
-    name VARCHAR(20),
-    release_date DATE,
+    artist VARCHAR(50),
+    name VARCHAR(50),
+    release_date YEAR,
     sales FLOAT,
-    genre VARCHAR(20)
+    genre VARCHAR(50),
+    PRIMARY KEY (id)
 );
+DESCRIBE album;
