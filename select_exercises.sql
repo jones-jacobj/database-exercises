ab3262ba7bef6f6a0eb74895388266ce32fd8589
J.J. Jones <jacob.j.jones01@gmail.com>
2022-12-14 09:02:57 -0600

    Updating

--- select_exercises.sql
+++ select_exercises.sql
@@ -1,3 +1,6 @@
 USE codeup_test_db;
 
+SELECT * FROM album where name LIKE '%';
 SELECT name FROM album where artist LIKE '%pink%';
+SELECT release_date FROM album where name LIKE 'Sgt. Pepper%';
+SELECT genre FROM album where name like 'nevermind';
