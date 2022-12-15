3e1d2508e348042bfd88603ac8d24add7a8cc72c
Jacob Jones <jacob.j.jones01@gmail.com>
2022-12-15 15:15:47 -0600

    Select_Exercises

--- select_exercises.sql
+++ select_exercises.sql
@@ -4,3 +4,5 @@ SELECT * FROM album where name LIKE '%';
 SELECT name FROM album where artist LIKE '%pink%';
 SELECT release_date FROM album where name LIKE 'Sgt. Pepper%';
 SELECT genre FROM album where name like 'nevermind';
+select name from album where release_date like '1990%';
+SELECT name FROM album WHERE genre LIKE 'Rock';
