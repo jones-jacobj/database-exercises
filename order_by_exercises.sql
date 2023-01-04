fd2681a103fc4a6cf9688f2633be814e1c799e5f
Jacob Jones <jacob.j.jones01@gmail.com>
2023-01-04 16:28:00 -0600

    Working

--- order_by_exercises.sql
+++ order_by_exercises.sql
@@ -1,5 +1,5 @@
 use employees;
-SELECT * FROM employees WHERE gender = 'm' AND first_name = 'Irena' OR first_name = 'Vidya' OR first_name = 'Maya';
+SELECT * FROM employees WHERE gender = 'm' AND first_name = 'Irena' OR first_name = 'Vidya' OR first_name = 'Maya' ORDER BY first_name;
 SELECT * FROM employees WHERE last_name LIKE 'e%';
 SELECT * FROM employees WHERE last_name LIKE '%q%';
 SELECT * FROM employees WHERE last_name LIKE 'e%' OR last_name LIKE '%e';
