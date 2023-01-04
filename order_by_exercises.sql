293047a6f92efbb0f8fcdb4edf61fe455878bd0e
Jacob Jones <jacob.j.jones01@gmail.com>
2023-01-04 17:19:40 -0600

    Did order by

--- order_by_exercises.sql
+++ order_by_exercises.sql
@@ -1,6 +1,6 @@
 use employees;
-SELECT * FROM employees WHERE gender = 'm' AND first_name = 'Irena' OR first_name = 'Vidya' OR first_name = 'Maya' ORDER BY first_name;
-SELECT * FROM employees WHERE last_name LIKE 'e%';
+SELECT * FROM employees WHERE gender = 'M' AND first_name = 'Irena' OR first_name = 'Vidya' OR first_name = 'Maya' ORDER BY last_name, first_name desc;
+SELECT * FROM employees WHERE last_name LIKE 'e%' ORDER BY emp_no desc;
 SELECT * FROM employees WHERE last_name LIKE '%q%';
 SELECT * FROM employees WHERE last_name LIKE 'e%' OR last_name LIKE '%e';
 SELECT * FROM employees WHERE last_name LIKE 'e%' AND last_name LIKE '%e';
